5b08f8614fda0be133832a0a0b1ec8e091adf84d
Python Whiz 256 <mostwonderfulboy@gmail.com>
2020-05-22 14:41:25 -0600

    Better Readme, cache, and support for large images

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ImageCraftifier
  Converts an image to Minecraft map instuctions
+## Usage
+ `python3 main.py file`
+ 1. A "Converting" progress bar will appear while it shifts the colors to map colors.
+ 2. The window will morph into a viewer. Use the mouse to pick a block, and scroll or drag to move around large images.
+## Info given
+![Diagram showing controls](gui-diagram.png)
+1. Block name(s)
+2. Map color
+3. RGB map color
+4. Slope: left side is northward block, right side is current block
+5. Position in image
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, ceil
 import pickle as pk
 from PIL import Image
 import numpy as np
@@ -8,6 +8,11 @@ import time
 from TextWrap import TextWrap
 import sys
 
+try:
+    import better_exceptions as be
+    sys.excepthook = be.excepthook
+except ImportError:
+    pass
 pygame.init()
 
 GUI_SCALE = 1
@@ -53,36 +58,46 @@ def test(colors):
     pygame.quit()
 
 def convert(data):
+    cache = {}
     print("Converting image...")
+    step = 512 / data.shape[0]
     win = pygame.display.set_mode((512,32))
+    pygame.display.set_caption("Converting...")
     # A map is 128x128
-    ret = np.empty((128,128), int)
+    ret = np.empty(data.shape[:2], int)
     w = 0
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
-            ret[i][j] = findClosestIndex(data[i][j])
-        w += 4
-        pygame.draw.rect(win,(0,255,0),(0,0,w,32))
+            if tuple(data[i][j]) in cache:
+                ret[i][j] = cache[tuple(data[i][j])]
+            else:
+                tmp = findClosestIndex(data[i][j])
+                ret[i][j] = tmp
+                cache[tuple(data[i][j])] = tmp
+        w += step
+        pygame.draw.rect(win,(0,255,0),(0,0,round(w),32))
         pygame.display.flip()
     return ret
         
 def fileconvert(filename):
     data = np.asarray(Image.open(filename).convert("RGB"))
-    if data.shape[:2] != (128,128):
-        raise Exception(f"File too small/big: {filename} ({data.shape[1]}x{data.shape[0]})")
+##    if data.shape[:2] != (128,128):
+##        raise Exception(f"File too small/big: {filename} ({data.shape[1]}x{data.shape[0]})")
     return convert(data)
 
 def scale(data):
     return type(data)([i*GUI_SCALE for i in data])
 
-def renderimage(surf, data):
+def renderimage(surf, data, pos):
     pxSize = 4*GUI_SCALE
     for i in range(128):
         for j in range(128):
-            
-            pygame.draw.rect(surf,tuple(
-                    [int(i) for i in dataExpanded[data[j][i]][0]]
-                ),(i*pxSize, j*pxSize, pxSize, pxSize))
+            x, y = i+pos[0], j+pos[1]
+            if x < data.shape[1] and y < data.shape[0]:
+                color = tuple([int(k) for k in dataExpanded[data[y][x]][0]])
+            else:
+                color = ((0,0,0) if (x+y)%2 == 0 else (127,127,127))
+            pygame.draw.rect(surf,color,(i*pxSize, j*pxSize, pxSize, pxSize))
 
 font = pygame.font.SysFont(
     [i for i in ["minecraftia",
@@ -91,31 +106,38 @@ font = pygame.font.SysFont(
           pygame.font.get_default_font()] if i in pygame.font.get_fonts()][0]
 , 16*GUI_SCALE)
 
-def renderblock(surf, data, selected):
-    color = tuple([int(i) for i in dataExpanded[data[selected[::-1]]][0]])
+def renderblock(surf, data, selected, pos):
+    if selected[0]+pos[0] >= data.shape[1] or selected[1]+pos[1] >= data.shape[0]:
+        return
+    dataBlock = dataExpanded[data[selected[1]+pos[1],selected[0]+pos[0]]]
+    color = tuple([int(i) for i in dataBlock[0]])
     pygame.draw.rect(surf, tuple([255-i for i in color]),
         scale((512+64, 80, 128, 128)))
     pygame.draw.rect(surf, color, scale((512+65, 81, 126, 126)))
     #print(selected[::-1])
-    name = dataExpanded[data[selected[::-1]]][1]
+    name = dataBlock[1]
     TextWrap(surf, (name if type(name) == str else ", ".join(name)),
             (255,255,255), scale((512, 0, 256, 80)), font)
     TextWrap(surf, "rgb(" + ", ".join([str(i) for i in color]) + ")",
             (255,255,255), scale((512, 224, 256, 80)), font)
-    slope = dataExpanded[data[selected[::-1]]][2]
+    slope = dataBlock[2]
     pygame.draw.line(surf, (255,255,255), *scale(
         ((512+64, 256+(slope*64)), (512+192, 384-(slope*64)))
     ), 10*GUI_SCALE)
+    TextWrap(surf, f"x:{selected[0]+1+pos[0]}, z:{selected[1]+1+pos[1]}",
+            (255,255,255), scale((512, 384, 256, 80)), font)
 
-def render(surf, data, selected):
+def render(surf, data, selected, pos):
     win.fill((0,0,0))
-    renderimage(surf, data)
+    renderimage(surf, data, pos)
     if selected != None:
-        pygame.draw.rect(surf, tuple(
-                [255-int(i) for i in dataExpanded[data[selected[::-1]]][0]]
-            ),
-            scale((selected[0]*4+1, selected[1]*4+1, 2, 2)))
-        renderblock(surf, data, selected)
+        if selected[0]+pos[0] < data.shape[1] and selected[1]+pos[1] < data.shape[0]:
+            pygame.draw.rect(surf, tuple(
+                    [255-int(i) for i in dataExpanded[data[selected[1]+pos[1],selected[0]+pos[0]]][0]]
+                ),
+                scale((selected[0]*4+1, selected[1]*4+1, 2, 2))
+            )
+        renderblock(surf, data, selected, pos)
 
 
 
@@ -123,34 +145,55 @@ def render(surf, data, selected):
 if len(sys.argv) != 2:
     print(f"Usage: {sys.argv[0]} file")
     sys.exit(1)
-        
+
+torange = lambda n, low, high: min(high, max(low, n))
+
 out = fileconvert(sys.argv[1])
-#out = pk.load(open("out.pkl","rb"))
 win = pygame.display.set_mode(((512+256)*GUI_SCALE, 512*GUI_SCALE))
 pygame.display.set_caption("ImageCraftifier")
-renderimage(win,out)
+renderimage(win,out,(0,0))
 pygame.display.flip()
-selected = (100,100)
-showDot = (100,100)
+selected = None
+showDot = None
 draw = True
 running = True
+pos = [0,0]
 while running:
     for event in pygame.event.get():
         #print(event)
-        if event.type == 4:
+        if event.type == 1: #Gain/lose focus
+            if event.gain == 0 and event.state == 1:
+                showDot = selected
+        if event.type == 4: #Move mouse
             if event.pos[0] < 512*GUI_SCALE:
                 showDot = tuple([i//(4*GUI_SCALE) for i in event.pos])
+                if event.buttons[0]:
+                    direction = [-i for i in event.rel]
+                    for i in range(2):
+                        pos[i] += direction[i] / (4*GUI_SCALE)
+                        pos[i] = torange(pos[i], 0, max(out.shape[1-i]-128,0))
                 draw = True
             else:
                 showDot = selected
-        if event.type == 5:
+        if event.type == 5: #Press mouse button
             if event.pos[0] < 512*GUI_SCALE:
-                showDot = selected = tuple([i//(4*GUI_SCALE) for i in event.pos])
-                draw = True
+                if event.button == 1:
+                    showDot = selected = tuple([i//(4*GUI_SCALE) for i in event.pos])
+                    draw = True
+                #  4
+                # 6 7
+                #  5
+                if event.button in range(4, 8):
+                    direction = [(0, -1), (0, 1), (-1, 0), (1, 0)][event.button - 4]
+                    for i in range(2):
+                        pos[i] += direction[i] * 16
+                        pos[i] = torange(pos[i], 0, max(ceil(out.shape[1-i]/16-8)*16,0))
+                    draw = True
+            
         if event.type == 12: #Quit
             running = False
     if draw:
-        render(win, out, showDot)
+        render(win, out, showDot, [round(i) for i in pos])
         pygame.display.flip()
         draw = False
 pygame.quit()
